31d6ff750f5720becc0651a8a65e436646ed5cc2
Ram <ramachandran.r@attinadsoftware.com>
2020-03-04 12:24:14 +0530

    brick runner can now take a proto input, it can be used in the case of the input brick where input has to be set to brick runner as its the starting point. now brick_runner.execute raises generic exception in case of proto being none

--- bricksdk/solution_runner/brick_runner/brick_runner.py
+++ bricksdk/solution_runner/brick_runner/brick_runner.py
@@ -7,8 +7,13 @@ class BrickRunner:
         self.brick_name = brick_name
         self.brick_config = brick_config
         self.result = None
+        self.proto = None
 
     def execute(self, *args, force_rerun=False, **kwargs):
         if force_rerun or self.result is None:
-            self.result = "{}({}_{})".format(self.brick_name, randint(0, 100), kwargs.get("proto", "input"))
+            self.proto = kwargs.get("proto", self.proto)
+            if self.proto:
+                self.result = "{}({}_{})".format(self.brick_name, randint(0, 100), self.proto)
+            else:
+                raise Exception("Proto cannot be none")
         return self.result
