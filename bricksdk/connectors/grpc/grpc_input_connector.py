e5ccab136cb3602170647fb682404eea4cfa39f7
Ram <ramachandran.r@attinadsoftware.com>
2020-03-18 11:07:33 +0530

    added a log when input connector is started

--- bricksdk/connectors/grpc/grpc_input_connector.py
+++ bricksdk/connectors/grpc/grpc_input_connector.py
@@ -14,18 +14,21 @@ class GrpcInputConnector(BaseInputConnector):
         self.brick_processor = brick_processor
         self.processor = Processor(proto_file_path)
         self.server = None
+        self.server_url = None
 
     def initialize(self, *args, **kwargs):
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         GrpcInterface = self._build_grpc_interface()
         self.processor.get_add_servicer_method()(GrpcInterface(), self.server)
-        self.server.add_insecure_port("[::]:{}".format(self.config.grpc.port))
+        self.server_url = "[::]:{}".format(self.config.grpc.port)
+        self.server.add_insecure_port(self.server_url)
 
     def listen(self, request, context):
         return self.brick_processor.process(request, context)
         # return request
 
     def start(self, *args, **kwargs):
+        print("Starting brick connector on {}".format(self.server_url))
         self.server.start()
         self.server.wait_for_termination()
 
