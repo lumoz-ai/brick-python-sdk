67157532a7a988fb4a30cdcefe8e1e44305c3bfe
ram <ramachandran.r@attinadsoftware.com>
2020-05-22 09:39:50 +0530

    updated the mains of templates

--- bricksdk/brick_commands/templates/generic/main.py
+++ bricksdk/brick_commands/templates/generic/main.py
@@ -1,9 +1,19 @@
-from bricksdk import BrickFactory
-from bricksdk.utils import Environments
+import json
+import os
 
 from brick.processor import BrickProcessor
 
-brick_factory = BrickFactory(environment=Environments.DEBUG_ENV).add_brick_processor(brick_processor=BrickProcessor())
+from bricksdk import BrickFactory
+from bricksdk.configurations import Configuration
+from bricksdk.utils import Environments
+
+json_configuration = None
+if os.environ.get("configuration"):
+    json_configuration = json.loads(os.environ.get("configuration"))
+configuration = Configuration(environment=Environments.DEBUG_ENV).load(json_configuration)
+
+brick_factory = BrickFactory(environment=Environments.DEBUG_ENV, configuration=configuration).add_brick_processor(
+    brick_processor=BrickProcessor())
 brick = brick_factory.create_brick()
 brick.initialize_components()
 brick.start()
